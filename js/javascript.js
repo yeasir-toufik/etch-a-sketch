3d15145c46cdcc3786a532126f25f6ff701ff1b6
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-04-07 17:45:03 +0600

    16*16 grid box completed.

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,13 @@
 console.log("js connectin working")
+
+
+let container = document.querySelector("#container");
+
+function makeDivs(numDivs) {
+  for (let d = 0; d < numDivs; d++) {
+    let cells = document.createElement('div');
+    container.appendChild(cells)
+  }
+}
+
+makeDivs(256);

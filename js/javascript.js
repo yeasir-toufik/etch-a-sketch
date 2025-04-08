0991f0ddd75e354ef5f9434787c0677a5eded029
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-04-08 21:26:54 +0600

    mouse hover feature added.

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-console.log("js connectin working")
+console.log("js connectin working");
 
 
 let container = document.querySelector("#container");
@@ -6,8 +6,41 @@ let container = document.querySelector("#container");
 function makeDivs(numDivs) {
   for (let d = 0; d < numDivs; d++) {
     let cells = document.createElement('div');
-    container.appendChild(cells)
+    cells.setAttribute("class", "gclass");
+    container.appendChild(cells);
   }
 }
 
 makeDivs(256);
+
+
+
+
+
+let hoverItem = document.querySelector(".gclass");
+
+hoverItem.addEventListener("mouseover", func, false);
+hoverItem.addEventListener("mouseout", func1, false);
+
+function func()
+{ 
+  hoverItem.setAttribute("style", "background-color:blue;")
+}
+
+function func1()
+{  
+  hoverItem.setAttribute("style", "background-color:green;")
+}
+
+
+document.body.addEventListener('mouseover', function (evt) {
+  if (evt.target.className === 'gclass') {
+      evt.target.setAttribute("style", "background-color:blue;");
+  }
+}, false);
+
+document.body.addEventListener('mouseout', function (evt) {
+  if (evt.target.className === 'gclass') {
+      evt.target.setAttribute("style", "background-color:red;");
+  }
+}, false);

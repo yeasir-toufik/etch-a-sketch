e20b843e490ba2ae38371f5cc6dddeab2adf470f
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-04-09 16:05:30 +0600

    assignment done.

--- js/javascript.js
+++ js/javascript.js
@@ -1,46 +1,46 @@
-console.log("js connectin working");
 
+function createGrid(n) {
+  
+  for (let i = 0; i < n; i++) {
+      let container = document.querySelector(".container");
+      let row = document.createElement("div");
+      row.style.display = "flex";
+      row.style.flex = "1";
+      container.appendChild(row);
 
-let container = document.querySelector("#container");
+      for (let j = 0; j < n; j++) {
+          let cell = document.createElement("div");
+          cell.style.border = "1px solid black";
+          cell.style.flex = "1";
+          row.appendChild(cell);
 
-function makeDivs(numDivs) {
-  for (let d = 0; d < numDivs; d++) {
-    let cells = document.createElement('div');
-    cells.setAttribute("class", "gclass");
-    container.appendChild(cells);
+          
+          cell.style.flex = "1";
+          cell.addEventListener("mouseover", function() {
+              if (cell.style.backgroundColor == "") {
+                  
+                  cell.style.backgroundColor = "red";
+              }
+              
+              else {
+                cell.style.backgroundColor = "green";
+              }
+          })
+          
+      }
   }
 }
 
-makeDivs(256);
+createGrid(16);
 
+let btn = document.querySelector("#btn");
 
-
-
-
-let hoverItem = document.querySelector(".gclass");
-
-hoverItem.addEventListener("mouseover", func, false);
-hoverItem.addEventListener("mouseout", func1, false);
-
-function func()
-{ 
-  hoverItem.setAttribute("style", "background-color:blue;")
-}
-
-function func1()
-{  
-  hoverItem.setAttribute("style", "background-color:green;")
-}
-
-
-document.body.addEventListener('mouseover', function (evt) {
-  if (evt.target.className === 'gclass') {
-      evt.target.setAttribute("style", "background-color:blue;");
+btn.addEventListener("click", function() {
+  let input;
+  while (isNaN(input) || input > 100) {
+      input = prompt("Please enter the grid dimensions size you want(Max 100)")
   }
-}, false);
-
-document.body.addEventListener('mouseout', function (evt) {
-  if (evt.target.className === 'gclass') {
-      evt.target.setAttribute("style", "background-color:red;");
-  }
-}, false);
+  let container = document.querySelector(".container");
+  container.replaceChildren();
+  createGrid(input);
+})
